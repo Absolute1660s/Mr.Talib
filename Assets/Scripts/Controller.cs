8ca0979b975696fd6cade660d9f1eef2abe8ecbf
Absolute1660s <138514054+Absolute1660s@users.noreply.github.com>
2024-01-08 18:22:27 +0400

    Yeni animasyon kodlari elave olundu

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,25 +8,40 @@ public class EvilWizardYonlendirmeKodu : MonoBehaviour
     public float speed;
     bool canJump;
     public float jumpForce;
+    private Animator anim;
 
-    void Start()
+
+    private void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             canJump = false;
         }
+
+
+
     }
 
     void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontal * speed * Time.deltaTime, rb.velocity.y);
+        if (horizontal == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
