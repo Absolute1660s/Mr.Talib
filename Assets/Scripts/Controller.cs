7872aef56f2d687d6c61defce1ac7b10aa521866
Absolute1660s <138514054+Absolute1660s@users.noreply.github.com>
2024-01-08 17:43:38 +0400

    Update Controller.cs
    yeni kod

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,48 +5,35 @@ using UnityEngine;
 public class EvilWizardYonlendirmeKodu : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float Speed;
-    bool jump;
+    public float speed;
+    bool canJump;
     public float jumpForce;
-    float horizontal;
+
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
-
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && jump == true)
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            rb.AddForce(new Vector2(0, jumpForce));
-            jump = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            canJump = false;
         }
     }
 
-
-
-
     void FixedUpdate()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector3 (horizontal * Time.deltaTime * Speed, rb.velocity.y, 0);
-
-
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed * Time.deltaTime, rb.velocity.y);
     }
-
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-            jump = true;
+            canJump = true;
         }
     }
-
-
-
-
-
 }
